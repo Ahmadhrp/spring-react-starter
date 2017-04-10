8ff23e3a95f359660334b3874f1c2740db1bdc3c
bensolo <aim10hrp@gmail.com>
2017-04-10 08:40:35 +0700

    Thymeleaf to React

---
+++ src/main/resources/static/js/Components/Navbar/navbar.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Bensolo on 4/8/2017.
+ */
+
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Navbar extends Component
+{
+    render()
+    {
+        return (
+            <nav className="navbar navbar-inverse navbar-fixed-top">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <Link className="navbar-brand" to="/">Working Report</Link>
+                    </div>
+                    <div id="navbar" className="navbar-collapse collapse">
+                        <ul className="nav navbar-nav navbar-right">
+                            <li>
+                                <Link to="/profile">Profile</Link>
+                            </li>
+                            <li><a href="#">Log out</a></li>
+                        </ul>
+                        <form className="navbar-form navbar-right">
+                            <input type="text" className="form-control" placeholder="Search..."/>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
